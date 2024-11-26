dd5636aac410e1391066005aed57ab635540f35c
Budara Chamudi <budara.chamudi@gmail.com>
2024-11-26 15:40:28 +0400

    Webstore Application

---
+++ products.js
@@ -0,0 +1,42 @@
+let products = [
+    {
+        "id": 1001,
+        "title": "Cat Food, 25lb bag",
+        "price": 20.00,
+        "image": "assets/image.png",
+        "availableInventory": 10,
+        "rating": 2
+    },
+    {
+        "id": 1002,
+        "title": "Interactive Cat Feather Toy",
+        "price": 5.99,
+        "image": "assets/2.jpg",
+        "availableInventory": 15,
+        "rating": 4
+    },
+    {
+        "id": 1003,
+        "title": "Self-Cleaning Kitty Litter Box",
+        "price": 49.99,
+        "image": "assets/3.jpg",
+        "availableInventory": 3,
+        "rating": 5
+    },
+    {
+        "id": 1004,
+        "title": "Catnip-Filled Plush Mouse Toy",
+        "price": 3.49,
+        "image": "assets/4.jpg",
+        "availableInventory": 12,
+        "rating": 4
+    },
+    {
+        "id": 1005,
+        "title": "Collapsible Cat Tunnel",
+        "price": 12.99,
+        "image": "assets/5.jpg",
+        "availableInventory": 8,
+        "rating": 4
+    }
+]
